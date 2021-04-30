035d509f49321e132ecb6afdbe2bb620c8381f4e
Thibault Duplessis <t@lichess.org>
2021-04-30 09:47:32 +0200

    show arena/swiss reason for denied entry - for #8812

--- app/views/tournament/side.scala
+++ app/views/tournament/side.scala
@@ -72,9 +72,10 @@ object side {
               p(
                 cls := List(
                   "condition text" -> true,
-                  "accepted"       -> v.verdict.accepted,
-                  "refused"        -> !v.verdict.accepted
-                )
+                  "accepted"  -> (ctx.isAuth && v.verdict.accepted),
+                  "refused"   -> (ctx.isAuth && !v.verdict.accepted)
+                ),
+                title := v.verdict.reason.map(_(ctx.lang))
               )(v.condition match {
                 case lila.tournament.Condition.TeamMember(teamId, teamName) =>
                   trans.mustBeInTeam(teamLink(teamId, teamName, withIcon = false))
