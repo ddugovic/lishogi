9d1b36b98f9a85fd21627689e57a80fe5bf4f775
Benedikt Werner <1benediktwerner@gmail.com>
2022-06-15 11:55:42 +0200

    api/tournament: Add teamMember condition and move conditions to sub-object

--- modules/tournament/src/main/JsonView.scala
+++ modules/tournament/src/main/JsonView.scala
@@ -143,12 +143,14 @@ final class JsonView(
           })
           .add("description" -> tour.description)
           .add("myUsername" -> me.map(_.username))
-          .add[Condition.RatingCondition]("minRating", tour.conditions.minRating)
-          .add[Condition.RatingCondition]("maxRating", tour.conditions.maxRating)
-          .add("minRatedGames", tour.conditions.nbRatedGame)
-          .add("onlyTitled", tour.conditions.titled.isDefined)
-          .add("teamMember", tour.conditions.teamMember.map(_.teamId))
-      }
+      } ++
+      tour.conditions.relevant ?? Json
+        .obj()
+        .add[Condition.RatingCondition]("minRating", tour.conditions.minRating)
+        .add[Condition.RatingCondition]("maxRating", tour.conditions.maxRating)
+        .add("nbRatedGame", tour.conditions.nbRatedGame)
+        .add("titled", tour.conditions.titled.isDefined)
+        .add("teamMember", tour.conditions.teamMember.map(_.teamId))
 
   def clearCache(tour: Tournament): Unit = {
     standingApi clearCache tour
