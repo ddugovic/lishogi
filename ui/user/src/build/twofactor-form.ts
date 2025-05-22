fb1d83c77f25fdb17c302a209cbd63aa4acfabbe
WandererXII <43043181+WandererXII@users.noreply.github.com>
2025-05-22 22:19:53 +0200

    Fix 2fa hostname on localhost

--- ui/user/src/build/twofactor-form.ts
+++ ui/user/src/build/twofactor-form.ts
@@ -1,7 +1,7 @@
 import QRCode from 'qrcode';
 
 window.lishogi.ready.then(() => {
-  const issuer = window.location.host; // lishogi.org
+  const issuer = encodeURIComponent(window.location.host); // lishogi.org
   const user = $(document.body).data('user');
   const secret = $('input[name=secret]').val();
 
