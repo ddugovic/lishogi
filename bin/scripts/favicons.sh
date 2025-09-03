1c4c216dbad4c684407dbb78680eed0d2c640d8b
WandererXII <43043181+WandererXII@users.noreply.github.com>
2025-09-03 03:58:18 +0200

    update favicons

--- bin/scripts/favicons.sh
+++ bin/scripts/favicons.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-file=public/logo/lishogi-favicon-1024.png
-thumb_file=$2
+file=../public/logo/lishogi-favicon-1024.png
 
-for px in 512 256 192 128 64; do
-  convert $file -scale ${px}x${px} public/logo/lishogi-favicon-${px}.png
+for px in 512 256 192 128 64 32; do
+  magick $file -scale ${px}x${px} ../public/logo/lishogi-favicon-${px}.png
+
+magick $file -scale 32x32 -channel RGB -negate +channel ../public/logo/lishogi-favicon-32-invert.png
+
 done
