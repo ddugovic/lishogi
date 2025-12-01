e134689bc145e72cde5bd302f8fa1d5af1665138
WandererXII <43043181+WandererXII@users.noreply.github.com>
2025-12-01 02:06:00 +0100

    Extract username from string in shogi/username

--- ui/analyse/src/player-bars.ts
+++ ui/analyse/src/player-bars.ts
@@ -1,9 +1,9 @@
 import { defined } from 'common/common';
 import * as game from 'game';
 import { colorName } from 'shogi/color-name';
-import { engineFromName, engineNameFromCode } from 'shogi/engine-name';
-import { extractRank, type Rank, rankFromRating } from 'shogi/rank';
-import { usernameVNodes } from 'shogi/username';
+import { engineNameFromCode } from 'shogi/engine-name';
+import { rankFromRating } from 'shogi/rank';
+import { type UsernameData, usernameDataFromName, usernameVNodes } from 'shogi/username';
 import { COLORS } from 'shogiops/constants';
 import { h, type VNode } from 'snabbdom';
 import { ops as treeOps } from 'tree';
@@ -11,24 +11,16 @@ import { renderClockOf } from './clocks';
 import type AnalyseCtrl from './ctrl';
 import { findTag } from './study/study-chapters';
 
-interface PlayerBarConfig {
-  color: Color;
-  name: string;
-  rank?: Rank;
-  engineLvl?: number;
-  bot?: boolean;
-}
-
 export function renderPlayerBars(ctrl: AnalyseCtrl): VNode[] | undefined {
   if (ctrl.embed || ctrl.forecast) return;
 
   const sente = findPlayer(ctrl, 'sente');
   const gote = findPlayer(ctrl, 'gote');
 
-  const noNames = ['', '?'];
+  const noNames = ['', '?', undefined];
   if (
-    noNames.includes(sente.name) &&
-    noNames.includes(gote.name) &&
+    noNames.includes(sente.username) &&
+    noNames.includes(gote.username) &&
     !treeOps.findInMainline(ctrl.tree.root, n => {
       return defined(n.clock) && !!n.id;
     })
@@ -36,94 +28,63 @@ export function renderPlayerBars(ctrl: AnalyseCtrl): VNode[] | undefined {
     return;
 
   return COLORS.map(color =>
-    renderPlayer(ctrl, color === 'sente' ? sente : gote, ctrl.bottomColor() !== color),
+    renderPlayer(ctrl, color === 'sente' ? sente : gote, color, ctrl.bottomColor() !== color),
   );
 }
 
-function renderPlayer(ctrl: AnalyseCtrl, player: PlayerBarConfig, top: boolean): VNode {
+function renderPlayer(
+  ctrl: AnalyseCtrl,
+  usernameData: UsernameData,
+  color: Color,
+  top: boolean,
+): VNode {
   return h(`div.player-bar.player-bar-${top ? 'top' : 'bot'}`, [
     h(
       'div.name-wrap',
       h(
-        `span.name.color-icon.${player.color}.is.text.user-link`,
+        `span.name.color-icon.${color}.is.text.user-link`,
         {
           attrs: {
-            title: colorName(player.color, ctrl.isHandicap()),
+            title: colorName(color, ctrl.isHandicap()),
           },
         },
-        usernameVNodes({
-          username: player.name,
-          bot: player.bot,
-          rank: player.rank,
-          engineLvl: player.engineLvl,
-        }),
+        usernameVNodes(usernameData),
       ),
     ),
     h(
       'div.clock-wrap',
-      ctrl.onMainline || ctrl.node.clock ? renderClockOf(ctrl, player.color) : undefined,
+      ctrl.onMainline || ctrl.node.clock ? renderClockOf(ctrl, color) : undefined,
     ),
   ]);
 }
 
-function findPlayer(ctrl: AnalyseCtrl, color: Color): PlayerBarConfig {
+function findPlayer(ctrl: AnalyseCtrl, color: Color): UsernameData {
   const study = ctrl.study;
 
   if (study) {
-    const tags = study.data.chapter.tags;
-
-    const name = findTag(tags, color);
-    const engine = name ? engineFromName(name) : undefined;
-
-    if (engine) {
-      return {
-        color,
-        name: engineNameFromCode(engine.code),
-        engineLvl: engine.level,
-      };
-    } else {
-      const isBot = name?.toLowerCase().startsWith('bot ');
-      const rank = !isBot ? extractRank(name) : undefined;
-      const nameWithoutPrefix =
-        (rank || isBot) && name ? name.slice(name.indexOf(' ') + 1 || 0) : name;
-      if (nameWithoutPrefix)
-        return {
-          color,
-          name: nameWithoutPrefix,
-          rank: rank,
-          bot: isBot,
-        };
-      else
-        return {
-          color,
-          name: '?',
-        };
-    }
+    const name = findTag(study.data.chapter.tags, color);
+    return usernameDataFromName(name);
   } else {
     const player = game.getPlayer(ctrl.data, color);
     const rank = !player.provisional && player.rating ? rankFromRating(player.rating) : undefined;
     if (player.user)
       return {
-        color,
-        name: player.user.username,
+        username: player.user.username,
         rank: rank,
         bot: player.user.title === 'BOT',
       };
     else if (player.ai)
       return {
-        color,
-        name: engineNameFromCode(player.aiCode),
+        username: engineNameFromCode(player.aiCode),
         engineLvl: player.ai,
       };
     else if (player.name || !ctrl.synthetic)
       return {
-        color,
-        name: player.name,
+        username: player.name,
       };
     else
       return {
-        color,
-        name: '?',
+        username: '?',
       };
   }
 }
--- ui/shogi/src/username.ts
+++ ui/shogi/src/username.ts
@@ -2,15 +2,18 @@ import type { MaybeVNodes } from 'common/snabbdom';
 import { flagImage } from 'common/snabbdom';
 import { i18n, i18nFormat } from 'i18n';
 import { h } from 'snabbdom';
-import { type Rank, rankTag } from './rank';
+import { engineFromName, engineNameFromCode } from './engine-name';
+import { extractRank, type Rank, rankTag } from './rank';
 
-export function usernameVNodes(data: {
+export interface UsernameData {
   username?: string;
   rank?: Rank;
   bot?: boolean;
   engineLvl?: number;
   countryCode?: string;
-}): MaybeVNodes {
+}
+
+export function usernameVNodes(data: UsernameData): MaybeVNodes {
   return [
     !data.bot && data.rank ? rankTag(data.rank) : undefined,
     data.engineLvl ? h('span.engine-lvl', `${i18nFormat('levelX', data.engineLvl)} `) : undefined,
@@ -18,4 +21,25 @@ export function usernameVNodes(data: {
     h(`span.name${!data.username ? '.anon' : ''}`, data.username || i18n('anonymousUser')),
     data.countryCode ? flagImage(data.countryCode) : undefined,
   ];
+}
+
+export function usernameDataFromName(name: string | undefined): UsernameData {
+  const engine = name ? engineFromName(name) : undefined;
+  if (engine)
+    return {
+      username: engineNameFromCode(engine.code),
+      engineLvl: engine.level,
+    };
+  else {
+    const isBot = !engine && name?.toLowerCase().startsWith('bot ');
+    const rank = !isBot && !engine ? extractRank(name) : undefined;
+    const nameWithoutPrefix =
+      (rank || isBot) && name ? name.slice(name.indexOf(' ') + 1 || 0) : name;
+
+    return {
+      username: nameWithoutPrefix,
+      rank: rank,
+      bot: isBot,
+    };
+  }
 }
